c7c6088aa80e701dc749eddc93904c080ef872c6
laure leter <laure.leter@gmail.com>
2021-03-01 19:48:50 +0100

    test on path

--- app.py
+++ app.py
@@ -18,6 +18,12 @@ import pytesseract
 custom_config = r'--oem 3 --psm 4'
 
 
+pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
+#image = Image.open(path)
+#t = pytesseract.image_to_string(image)
+#line_bot_api.reply_message(event.reply_token,TextSendMessage(text=t))
+
+
 
 
 st.write('hello 👋')
